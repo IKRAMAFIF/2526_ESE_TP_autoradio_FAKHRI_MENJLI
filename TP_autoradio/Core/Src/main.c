c40badd0dbc86db70c993dd338d58020ae0dee7f
IKRAMAFIF <ikram.afif@ensea.fr>
2025-12-05 17:34:16 +0100

    3.5 Bypass numérique

--- TP_autoradio/Core/Src/main.c
+++ TP_autoradio/Core/Src/main.c
@@ -43,7 +43,16 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+extern SPI_HandleTypeDef hspi3;
 
+#define AUDIO_BLOCK_SIZE 24
+#define AUDIO_BUFFER_SIZE (AUDIO_BLOCK_SIZE * 4)
+
+// Defines for triangular wave generation
+#define SAMPLE_RATE_HZ 48000
+#define TRIANGLE_TEST_FREQ_HZ 440
+#define TRIANGLE_MAX_AMPLITUDE 15000
+#define TRIANGLE_STEP ((2 * TRIANGLE_MAX_AMPLITUDE) / (SAMPLE_RATE_HZ / TRIANGLE_TEST_FREQ_HZ))
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -54,12 +63,22 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+
 uint8_t rxbuffer;
 SemaphoreHandle_t uartRxSemaphore;
 h_shell_t h_shell;
-extern SPI_HandleTypeDef hspi3;
 LED_Driver_t led_driver;
 h_sgtl5000_t sgtl5000_handle;
+uint8_t rxCharBuffer;
+/********* Question 3.3 – Buffers I2S *********/
+uint16_t audio_tx_buffer[AUDIO_BUFFER_SIZE];
+uint16_t audio_rx_buffer[AUDIO_BUFFER_SIZE];
+
+// Global variables for triangle wave generation
+int16_t triangle_current_value = 0;
+int8_t triangle_direction = 1;
+
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -77,7 +96,6 @@ int __io_putchar(int chr)
 	HAL_UART_Transmit(&huart2, (uint8_t*) &chr, 1, HAL_MAX_DELAY);
 	return chr;
 }
-
 
 //=======QUESTION 6======
 /*TaskHandle_t h_shell_task;
@@ -162,51 +180,55 @@ void task_read_CHIP_ID(void *unused) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* Configure the peripherals common clocks */
-  PeriphCommonClock_Config();
+	/* Configure the peripherals common clocks */
+	PeriphCommonClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_USART2_UART_Init();
-  MX_SPI3_Init();
-  MX_I2C2_Init();
-  MX_SAI2_Init();
-  /* USER CODE BEGIN 2 */
-  __HAL_SAI_ENABLE(&hsai_BlockA2);
-    sgtl5000_handle.hi2c = &hi2c2;
-    sgtl5000_handle.i2c_address = SGTL5000_I2C_ADDR_WRITE;
-    sgtl5000_init(&sgtl5000_handle);
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_USART2_UART_Init();
+	MX_SPI3_Init();
+	MX_I2C2_Init();
+	MX_SAI2_Init();
+	/* USER CODE BEGIN 2 */
 
-    // question 6
+	__HAL_SAI_ENABLE(&hsai_BlockA2);
+	sgtl5000_handle.hi2c = &hi2c2;
+	sgtl5000_handle.i2c_address = SGTL5000_I2C_ADDR_WRITE;
+	sgtl5000_init(&sgtl5000_handle);
+
+	// Start SAI DMA transfers
+	HAL_SAI_Transmit_DMA(&hsai_BlockA2, (uint8_t*)audio_tx_buffer, AUDIO_BUFFER_SIZE);
+	HAL_SAI_Receive_DMA(&hsai_BlockB2, (uint8_t*)audio_rx_buffer, AUDIO_BUFFER_SIZE);
 	/*if (xTaskCreate(ShellTask,"shell", 256, NULL,1, &h_shell_task)!=pdPASS)
      {
+
       printf("error creation task shell\r\n");
       Error_Handler();
      }
@@ -235,158 +257,243 @@ int main(void)
 
 
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Call init function for freertos objects (in cmsis_os2.c) */
-  MX_FREERTOS_Init();
+	/* Call init function for freertos objects (in cmsis_os2.c) */
+	MX_FREERTOS_Init();
 
-  /* Start scheduler */
-  osKernelStart();
+	/* Start scheduler */
+	osKernelStart();
 
-  /* We should never get here as control is now taken by the scheduler */
+	/* We should never get here as control is now taken by the scheduler */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 
 		//Blink_LEDs();
 		//LED_Chenillard();
 		//led_driver.blink_all();
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Configure the main internal regulator output voltage
+	 */
+	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 1;
+	RCC_OscInitStruct.PLL.PLLN = 10;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief Peripherals Common Clock Configuration
-  * @retval None
-  */
+ * @brief Peripherals Common Clock Configuration
+ * @retval None
+ */
 void PeriphCommonClock_Config(void)
 {
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
-  PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
-  PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
-  PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
-  PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
-  PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
-  PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
-  PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
-  PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the peripherals clock
+	 */
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
+	PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
+	PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
+	PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+	PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
+	PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
+	PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+	PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+	PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART2)
+	{
+		BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
+		xSemaphoreGiveFromISR(uartRxSemaphore, &xHigherPriorityTaskWoken);
+
+		HAL_UART_Receive_IT(&huart2, &rxCharBuffer, 1);
+
+		portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+	}
+}
+
+// SAI Transmit Half-Complete Callback
+void HAL_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	/*if (hsai->Instance == SAI2_Block_A)
+	{
+		for (int i = 0; i < AUDIO_BLOCK_SIZE; i++) {
+			triangle_current_value += triangle_direction * TRIANGLE_STEP;
+
+			if (triangle_current_value >= TRIANGLE_MAX_AMPLITUDE) {
+				triangle_current_value = TRIANGLE_MAX_AMPLITUDE; // Cap at max
+				triangle_direction = -1; // Change direction to falling
+			} else if (triangle_current_value <= -TRIANGLE_MAX_AMPLITUDE) {
+				triangle_current_value = -TRIANGLE_MAX_AMPLITUDE; // Cap at min
+				triangle_direction = 1; // Change direction to rising
+			}
+			uint16_t output_sample = (uint16_t)triangle_current_value;
+
+			audio_tx_buffer[(2 * i)] = output_sample; // Left Channel
+			audio_tx_buffer[(2 * i) + 1] = output_sample; // Right Channel
+		}
+	}*/
+}
+
+// SAI Transmit Complete Callback
+void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	/*if (hsai->Instance == SAI2_Block_A)
+	{
+		for (int i = 0; i < AUDIO_BLOCK_SIZE; i++) {
+			triangle_current_value += triangle_direction * TRIANGLE_STEP;
+
+			if (triangle_current_value >= TRIANGLE_MAX_AMPLITUDE) {
+				triangle_current_value = TRIANGLE_MAX_AMPLITUDE;
+				triangle_direction = -1;
+			} else if (triangle_current_value <= -TRIANGLE_MAX_AMPLITUDE) {
+				triangle_current_value = -TRIANGLE_MAX_AMPLITUDE;
+				triangle_direction = 1;
+			}
+
+			uint16_t output_sample = (uint16_t)triangle_current_value;
+
+			audio_tx_buffer[AUDIO_BLOCK_SIZE * 2 + (2 * i)] = output_sample;
+			audio_tx_buffer[AUDIO_BLOCK_SIZE * 2 + (2 * i) + 1] = output_sample;
+		}
+	}*/
+}
+
+
+void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	if (hsai->Instance == SAI2_Block_B)
+	{
+		// Process the first half of the RX buffer
+		// Copy first half of RX buffer to first half of TX buffer
+		for (int i = 0; i < AUDIO_BLOCK_SIZE * 2; i++) {
+			audio_tx_buffer[i] = audio_rx_buffer[i];
+		}
+	}
+}
+
+// SAI Receive Complete Callback
+void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	if (hsai->Instance == SAI2_Block_B)
+	{
+		// Process the second half of the RX buffer
+		// Copy second half of RX buffer to second half of TX buffer
+		for (int i = 0; i < AUDIO_BLOCK_SIZE * 2; i++) {
+			audio_tx_buffer[AUDIO_BLOCK_SIZE * 2 + i] = audio_rx_buffer[AUDIO_BLOCK_SIZE * 2 + i];
+		}
+	}
+}
 /* USER CODE END 4 */
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM7 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM7 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  /* USER CODE BEGIN Callback 0 */
+	/* USER CODE BEGIN Callback 0 */
 
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM7) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM7) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
 
-  /* USER CODE END Callback 1 */
+	/* USER CODE END Callback 1 */
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- TP_autoradio/Core/Src/sgtl5000.c
+++ TP_autoradio/Core/Src/sgtl5000.c
@@ -60,7 +60,7 @@ HAL_StatusTypeDef sgtl5000_init(h_sgtl5000_t *h_sgtl5000) {
 	HAL_StatusTypeDef ret = HAL_OK;
 	uint16_t chip_id_value;
 
-	printf("SGTL5000: Starting initialization...\r\n");
+	printf("SGTL5000: Starting initialization\r\n");
 
 	// --- 1. Read CHIP_ID to verify communication ---
 	ret = sgtl5000_i2c_read_register(h_sgtl5000, SGTL5000_CHIP_ID, &chip_id_value);
@@ -70,59 +70,45 @@ HAL_StatusTypeDef sgtl5000_init(h_sgtl5000_t *h_sgtl5000) {
 	}
 	printf("SGTL5000: CHIP_ID = 0x%04X \r\n", chip_id_value);
 
-
-	// Power up LINEOUT, HP, ADC, DAC, REFTOP, VAG (from ANA_POWER register)
-	// Value: 0x6AFF (bits: LINEOUT_POWERUP, ADC_POWERUP, CAPLESS_HEADPHONE_POWERUP, DAC_POWERUP, HEADPHONE_POWERUP, REFTOP_POWERUP)
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_ANA_POWER, 0x6AFF);
 	printf("SGTL5000: CHIP_ANA_POWER set to 0x6AFF\r\n");
 
-	// Configure charge pump to use VDDIO rail (bit 5 and bit 6 of LINREG_CTRL)
-	// This is needed if VDDA and VDDIO are > 3.1V (e.g., 3.3V)
 	ret |= sgtl5000_i2c_set_bit(h_sgtl5000, SGTL5000_CHIP_LINREG_CTRL, (1 << 5) | (1 << 6));
 	printf("SGTL5000: CHIP_LINREG_CTRL charge pump configured\r\n");
 
-	// Set ground, ADC, DAC reference voltage (VDDA/2) and bias current.
-	// Assuming VDDA = 3.3V, VDDA/2 is approx 1.65V. The default 0.8V is 0x0118.
-	// For 1.575V VAG_VAL (0x01FF) and BIAS_CTRL = -50% (bit 1) and SMALL_POP = 1 (bit 0)
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_REF_CTRL, 0x01FF);
 	printf("SGTL5000: CHIP_REF_CTRL set to 0x01FF\r\n");
 
-	// Set LINEOUT reference voltage to VDDIO/2 (1.65 V) and bias current to 0.36 mA
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_LINE_OUT_CTRL, 0x031E);
 	printf("SGTL5000: CHIP_LINE_OUT_CTRL set to 0x031E\r\n");
 
-	// Enable short detect mode for headphone (HP_OUT)
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_SHORT_CTRL, 0x1106);
 	printf("SGTL5000: CHIP_SHORT_CTRL set to 0x1106\r\n");
 
-	// Enable Zero-cross detect for HP_OUT and ADC (CHIP_ANA_CTRL)
-	// And set ADC input to Line-In
-	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_ANA_CTRL, 0x0004); // SELECT_ADC = LINEIN
+	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_ANA_CTRL, 0x0004);
 	printf("SGTL5000: CHIP_ANA_CTRL set to 0x0004 (ADC from Line-In)\r\n");
 
-	// Power up desired digital blocks: I2S_IN, I2S_OUT, DAC, ADC
+
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_DIG_POWER, 0x0073);
 	printf("SGTL5000: CHIP_DIG_POWER set to 0x0073\r\n");
 
-	// --- 3. System MCLK and Sample Clock ---
+	// System MCLK and Sample Clock ---
 
 	// Configure SYS_FS (sample rate) to 48 kHz. MCLK_FREQ is for 256*Fs
-	// For 48kHz, SYS_FS = 0x0002 (bits 3:2)
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_CLK_CTRL, 0x0004); // SYS_FS = 48kHz (bit 2 is 1)
 	printf("SGTL5000: CHIP_CLK_CTRL set to 0x0004 (48kHz SYS_FS)\r\n");
 
 	// Configure I2S interface in slave mode (SGTL5000 acts as slave to STM32)
-	// I2S_MS=0 (Slave), I2S_DLEN=16 bits (0x10)
 	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_I2S_CTRL, 0x0130);
 	printf("SGTL5000: CHIP_I2S_CTRL set to 0x0130 (I2S Slave, 16-bit)\r\n");
 
-	// --- 4. Input/Output Routing ---
+	// Input/Output Routing ---
 
-	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_ADCDAC_CTRL, 0x0000); // DAC_MUTE = 0
+	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_ADCDAC_CTRL, 0x0000);
 	printf("SGTL5000: CHIP_ADCDAC_CTRL set to 0x0000 (DAC Unmuted)\r\n");
 
 	// Set DAC volume (0dB, max)
-	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_DAC_VOL, 0x3C3C); // 0x3C = 0dB
+	ret |= sgtl5000_i2c_write_register(h_sgtl5000, SGTL5000_CHIP_DAC_VOL, 0x3C3C);
 	printf("SGTL5000: CHIP_DAC_VOL set to 0x3C3C (0dB)\r\n");
 
 	if (ret != HAL_OK) {
